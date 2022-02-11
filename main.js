e78a97d7e2224189578aeaead06d4f693a69b5a2
havenfricke <robslaven@live.com>
2022-02-10 22:32:00 -0700

    2.10.22

--- main.js
+++ main.js
@@ -24,16 +24,23 @@ let plants = {
 function drawPlants() {
   let template = ''
   for (let key in plants) {
-    let water = plants[key]
+    let care = plants[key]
     template += `<span class="col-2 selectable">
     <h1>🌳</h1>
-    <div class="progress" style="height: 5px rounded;">
-      <div onclick="giveWater()" class="progress-bar" role="progressbar" style="width: ${}%;" aria-valuenow="25"
-        aria-valuemin="0" aria-valuemax="100"></div>
-        <div onclick="giveWater()" class="progress-bar" role="progressbar" style="width: ${}%;" aria-valuenow="25"
-        aria-valuemin="0" aria-valuemax="100"></div>
-        <div onclick="giveWater()" class="progress-bar" role="progressbar" style="width: ${}%;" aria-valuenow="25"
-        aria-valuemin="0" aria-valuemax="100"></div>
+    <div class="progress" style="height: 5px;">
+    Water
+      <div onclick="giveWater()" class="progress-bar" role="progressbar" style="width: ${care.water}%;" aria-valuenow="25"
+        aria-valuemin="0" aria-valuemax="100">${care.water}</div>
+    Fertilizer
+        <div onclick="giveWater()" class="progress-bar" role="progressbar" style="width: ${care.fertilizer}%;" aria-valuenow="25"
+        aria-valuemin="0" aria-valuemax="100">${care.fertilizer}</div>
+    Love
+        <div onclick="giveWater()" class="progress-bar" role="progressbar" style="width: ${care.love}%;" aria-valuenow="25"
+        aria-valuemin="0" aria-valuemax="100">${care.love}</div>
+    </div>
     </div>`
   }
+  document.getElementById('plant-row').innerHTML = template
 }
+
+drawPlants()
