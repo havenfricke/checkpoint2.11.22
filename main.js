a71abc6a3a44381f8e0edf6963ca3d7f0411418a
havenfricke <robslaven@live.com>
2022-02-11 18:19:47 -0700

    2.11.22 619pm

--- main.js
+++ main.js
@@ -6,13 +6,40 @@ let plant = {
   }
 }
 
-let myFruit = 0
+let myDict = {
+  myThings: {
+    fruit: 0,
+    fruitMultiplier: 0,
+    fruitWorker: 0,
+    fruitFertilizer: 0,
+    fruitTools: 0
+  }
+}
+
+
+
 
 
 function getFruit() {
-  myFruit += 2
-  console.log(myFruit);
+  myDict.myThings.fruit += 1
+  console.log(myDict.myThings.fruit);
+  drawFruit()
 }
+
+function drawFruit() {
+  let template = ''
+  for (let key in myDict) {
+    let fruitCount = myDict[key]
+    template += `
+    <span class="col-12 text-center p-1">
+        <h2>FRUIT: ${fruitCount.fruit}</h2>
+      </span>`
+  }
+  document.getElementById('fruit-count').innerHTML = template
+}
+
+drawFruit()
+
 
 
 
